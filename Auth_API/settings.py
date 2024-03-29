abbb0609e0067e8069ecfd30fa514d3dda84a160
Jay <jay@gmail.com>
2024-03-29 10:22:22 -0400

    "PH-03"  MySQL Database Added

--- Auth_API/settings.py
+++ Auth_API/settings.py
@@ -76,11 +76,23 @@ WSGI_APPLICATION = 'Auth_API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "USER": "root",
+        "NAME": "Auth_Project",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+
+    },
 }
 
 
